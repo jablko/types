1cde6dd6328102d33e3221d638247d554cabcd93
Jack Bates <jack@nottheoilrig.com>
2022-05-23 15:37:22 -0700

    test: check actual examples from the npm registry

---
+++ test/fixtures/tiny-tarball@1.0.0-manifest.ts
@@ -0,0 +1,26 @@
+import * as pacote from "pacote";
+import { PackumentVersion } from "../../";
+
+export const metadata: PackumentVersion & pacote.ManifestResult = {
+  name: "tiny-tarball",
+  version: "1.0.0",
+  description: "tiny tarball used for health checks",
+  main: "index.js",
+  scripts: { test: 'echo "Error: no test specified" && exit 1' },
+  author: { name: "Ben Coe", email: "ben@npmjs.com" },
+  license: "ISC",
+  _id: "tiny-tarball@1.0.0",
+  _shasum: "bbf102d5ae73afe2c553295e0fb02230216f65b1",
+  _from: "tiny-tarball@1.0.0",
+  _npmVersion: "2.7.0",
+  _nodeVersion: "1.5.0",
+  _npmUser: { name: "bcoe", email: "bencoe@gmail.com" },
+  maintainers: [{ name: "bcoe", email: "bencoe@gmail.com" }],
+  dist: {
+    shasum: "bbf102d5ae73afe2c553295e0fb02230216f65b1",
+    tarball: "https://registry.npmjs.org/tiny-tarball/-/tiny-tarball-1.0.0.tgz",
+  },
+  directories: {},
+  _resolved: "https://registry.npmjs.org/tiny-tarball/-/tiny-tarball-1.0.0.tgz",
+  _integrity: "sha1-u/EC1a5zr+LFUyleD7AiMCFvZbE=",
+};
